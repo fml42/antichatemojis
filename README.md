37360154494e97e35da18a961203be96a27841fa
fml42 <88512759+fml42@users.noreply.github.com>
2023-01-02 06:05:04 +0100

    add links fix

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 minecraft mod for fabric that prevents auto conversion of ascii emotes like :) to an emoji on a server with a plugin that does that (like EmojiChat) by inserting zero width non-joiners between the characters
 
+also prevents auto conversion of links by inserting zero width non-joiners around periods, to prevent phrases like "e.g." and "i.e." to become links. words beginning with http are not affected
+
 this is a hacky fix but w/e
--- src/main/java/net/fml42/antichatemojis/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/net/fml42/antichatemojis/mixin/ClientPlayNetworkHandlerMixin.java
@@ -9,22 +9,36 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public class ClientPlayNetworkHandlerMixin {
 
 	private static final String[] EMOJIS = {
-			":)",";)", ":(",";(", ":D", "xD", "XD", "xP", "XP", ":|", ":p", ":P", ":/", "D:", "<3"
+			":)",";)", ":(",";(", ":D", "xD", "XD", "xP", "XP", ":|", ":p", ":P", ":/", "D:", "<3",
+			//":o", ":O", ":0", ":>", ":<",
 	};
 	private static final String DECORATOR = "\u200c";
+	private static final String WORDS_SEPARATOR = " ";
 
 	@ModifyVariable(at = @At("HEAD"), method = "sendChatMessage(Ljava/lang/String;)V", ordinal = 0)
 	private String init(String content) {
 		//AntiChatEmojis.LOGGER.info("chat message: "+content);
 
-		for (String emoji: EMOJIS) {
-			if (content.contains(emoji)) {
-				String emojiDecorated = decorate(emoji);
-				content = content.replace(emoji, emojiDecorated);
+		//remove links
+		String[] words = content.split(WORDS_SEPARATOR);
+		for (int i = 0; i<words.length; i++) {
+			boolean isLink = words[i].startsWith("http");
+
+			if (!isLink) {
+				//break links
+				words[i] = words[i].replace(".", DECORATOR+".");
+
+				//break emojis
+				for (String emoji: EMOJIS) {
+					if (words[i].contains(emoji)) {
+						String emojiDecorated = decorate(emoji);
+						words[i] = words[i].replace(emoji, emojiDecorated);
+					}
+				}
 			}
 		}
 
-		return content;
+		return String.join(WORDS_SEPARATOR, words);
 	}
 
 	private String decorate(String str) {
